36d742787a5d006da148acf324209cc4edee75d9
jiimms <jiimms@yahoo.com>
2016-04-21 00:25:31 +0530

    add signup path to signup button that takes to new user page.done with signup branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  
+
   root 'static_pages#home'
   get "help" => 'static_pages#help'
   get "contact" => 'static_pages#contact'
+  get "signup" =>'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
